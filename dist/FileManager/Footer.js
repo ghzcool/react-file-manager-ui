77822e61ebdb0178af79adb2e7054c2d7ca86790
Svetoslav Nedkov <sveto.n@gmail.com>
2023-10-17 19:40:46 +0200

    Build project for new features

--- dist/FileManager/Footer.js
+++ dist/FileManager/Footer.js
@@ -9,6 +9,8 @@ var _react = _interopRequireDefault(require("react"));
 
 var _fa = require("react-icons/fa");
 
+var _Utils = require("./Utils");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function Footer(_ref) {
@@ -21,13 +23,52 @@ function Footer(_ref) {
     reload,
     rename,
     labels,
-    enabledFeatures
+    enabledFeatures,
+    getDownloadLink,
+    getFileSizeBytes,
+    getFileChangedDate
   } = _ref;
   const list = structure[currentPath] || [];
-  const files = list.filter(item => item.type === 1).length;
-  const folders = list.filter(item => item.type === 2).length;
-  const folderLabel = folders > 1 ? labels['folderMultiple'] : labels['folderSingle'];
-  const fileLabel = files > 1 ? labels['fileMultiple'] : labels['fileSingle'];
+  const footerText = getFooterText();
+
+  function getFooterText() {
+    const files = list.filter(item => item.type === 1).length;
+    const folders = list.filter(item => item.type === 2).length;
+    const folderLabel = folders > 1 ? labels['folderMultiple'] : labels['folderSingle'];
+    const fileLabel = files > 1 ? labels['fileMultiple'] : labels['fileSingle'];
+    let text = "".concat(folders, " ").concat(folderLabel, " and ").concat(files, " ").concat(fileLabel);
+    const selectionItem = selection[0];
+
+    if (selectionItem) {
+      let selectionBytes;
+
+      if (enabledFeatures.indexOf('getFileSizeBytes') !== -1) {
+        selectionBytes = (0, _Utils.humanReadableByteCount)(getFileSizeBytes(selectionItem.item));
+      }
+
+      let selectionDate;
+
+      if (enabledFeatures.indexOf('getFileChangedDate') !== -1) {
+        selectionDate = new Date(getFileChangedDate(selectionItem.item)).toLocaleString();
+      }
+
+      text += " - ".concat((0, _Utils.stripLeadingDirectories)(selectionItem), ": ");
+
+      if (selectionBytes) {
+        text += "".concat(selectionBytes);
+      }
+
+      if (selectionDate) {
+        if (selectionBytes) {
+          text += ', ';
+        }
+
+        text += " ".concat(labels['lastChangedLabel'], " ").concat(selectionDate);
+      }
+    }
+
+    return text;
+  }
 
   const onDeletePath = () => {
     deletePaths(selection).then(() => {
@@ -44,7 +85,7 @@ function Footer(_ref) {
     className: "FileManager-Footer"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "Footer-Left"
-  }, folders, " ", folderLabel, " and ", files, " ", fileLabel), /*#__PURE__*/_react.default.createElement("div", {
+  }, footerText), /*#__PURE__*/_react.default.createElement("div", {
     className: "Footer-Right"
   }, selection.length === 1 && enabledFeatures.indexOf('rename') !== -1 && /*#__PURE__*/_react.default.createElement("button", {
     className: "Icon-Button",
@@ -56,5 +97,11 @@ function Footer(_ref) {
     type: "button",
     onClick: () => onDeletePath(),
     title: labels['delete']
-  }, /*#__PURE__*/_react.default.createElement(_fa.FaTrash, null))));
+  }, /*#__PURE__*/_react.default.createElement(_fa.FaTrash, null)), !!selection.length && enabledFeatures.indexOf('getDownloadLink') !== -1 && /*#__PURE__*/_react.default.createElement("a", {
+    href: getDownloadLink(selection),
+    download: (0, _Utils.stripLeadingDirectories)(selection[0]),
+    className: "Icon-Button",
+    type: "button",
+    title: labels['download']
+  }, /*#__PURE__*/_react.default.createElement(_fa.FaDownload, null))));
 }
